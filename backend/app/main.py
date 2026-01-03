5c5173d764ee40e287218d9496da2b383de18f70
jonathanliu72 <jonathan.liu72@gmail.com>
2026-01-03 15:35:56 -0500

    email functionality

---
+++ backend/app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routers import health, email
+
+app = FastAPI()
+
+# Set up CORS
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(health.router)
+app.include_router(email.router)
